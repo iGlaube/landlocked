c67e193b067d12fb5ffd71f17767caa612c56a2e
iGlaube <b.campbell787@gmail.com>
2020-01-07 21:46:12 +0000

    Updates

---
+++ precache-manifest.6ceb5e430a4a38a4b748a8fcb90f0268.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa0a8fa1fb040c598bfa37cb0e6e3ddf",
+    "url": "/landlocked/index.html"
+  },
+  {
+    "revision": "ebb9d5b725dbd39cca55",
+    "url": "/landlocked/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "299b4f744761fc2a9876",
+    "url": "/landlocked/static/js/2.bfaaff57.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/landlocked/static/js/2.bfaaff57.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ebb9d5b725dbd39cca55",
+    "url": "/landlocked/static/js/main.4367d4b2.chunk.js"
+  },
+  {
+    "revision": "fae33a13c6177778dd14",
+    "url": "/landlocked/static/js/runtime-main.487b8289.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/landlocked/static/media/logo.5d5d9eef.svg"
+  }
+]);
